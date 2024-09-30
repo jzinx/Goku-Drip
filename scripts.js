9c1941ad42b5aa19f3203c43b744af92b4c454f7
jzinx <132531992+jzinx@users.noreply.github.com>
2024-09-30 12:49:27 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Função para trocar a imagem principal ao clicar nas miniaturas
+function changeImage(imageSrc) {
+    const mainImage = document.getElementById('main-image');
+    mainImage.src = imageSrc;
+  }
+  
